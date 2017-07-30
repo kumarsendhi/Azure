6e364b7f9a0c489d88e0d470b5ef21dd5af6eaaa
kumarsendhi <kumar.sendhi@gmail.com>
2017-07-30 20:32:28 +0530

    first checkin

---
+++ Labs/Completed/Building an Enterprise Web App with Azure and Visual Studio 2017/EventsSolution/Events/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+using System.Configuration;
+using StackExchange.Redis;
+
+
+namespace Events
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static ConnectionMultiplexer redisCache;
+        public static ConnectionMultiplexer RedisCache
+        {
+            get
+            {
+                if (redisCache == null || !redisCache.IsConnected)
+                {
+                    redisCache = ConnectionMultiplexer.Connect(ConfigurationManager.ConnectionStrings["RedisConnection"].ConnectionString);
+                }
+                return redisCache;
+            }
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var strategy = new FixedInterval("fixed", 10, TimeSpan.FromSeconds(3));
+            var strategies = new List<RetryStrategy> { strategy };
+            var manager = new RetryManager(strategies, "fixed");
+            RetryManager.SetDefault(manager);
+        }
+    }
+}
